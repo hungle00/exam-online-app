a33bede831be5fcb6a06486dc38eacea885f24d4
hungle00 <hungle2048@gmail.com>
2021-05-17 10:37:53 +0700

    Add reset password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters    
     new_attrs = [:username, :email, :password, :password_confirmation, :remember_me] 
-    update_attrs = [:username, :email, :avatar, :remember_me]
+    update_attrs = [:username, :email, :password, :password_confirmation, :avatar, :remember_me]
     devise_parameter_sanitizer.permit :sign_up, keys: new_attrs            
     devise_parameter_sanitizer.permit :account_update, keys: update_attrs    
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }
-  validate :validate_username
+  #validate :validate_username
   has_one_attached :avatar
 
   has_many :notifications
