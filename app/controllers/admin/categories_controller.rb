c50bf309a4b869df1a69a75b975917a650b2a46b
hungle00 <hungle2048@gmail.com>
2021-05-19 22:23:20 +0700

    fix indent, remove json, edit homepage

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -6,28 +6,20 @@ class Admin::CategoriesController < ApplicationController
     # POST /categories or /categories.json
     def create
       @category = Category.new(category_params)
-  
-      respond_to do |format|
-        if @category.save
-          format.html { redirect_to @category, notice: "Category was successfully created." }
-          format.json { render :show, status: :created, location: @category }
-        else
-          format.html { render :new, status: :unprocessable_entity }
-          format.json { render json: @category.errors, status: :unprocessable_entity }
-        end
+
+      if @category.save
+        redirect_to @category, notice: "Category was successfully created."
+      else
+        redirect_back fallback_location: categories_url, notice: "Category must not blank!" 
       end
     end
   
     # PATCH/PUT /categories/1 or /categories/1.json
     def update
-      respond_to do |format|
-        if @category.update(category_params)
-          format.html { redirect_to @category, notice: "Category was successfully updated." }
-          format.json { render :show, status: :ok, location: @category }
-        else
-          format.html { render :edit, status: :unprocessable_entity }
-          format.json { render json: @category.errors, status: :unprocessable_entity }
-        end
+      if @category.update(category_params)
+        redirect_to @category, notice: "Category was successfully updated."
+      else
+        redirect_back fallback_location: @category, notice: "Category must not blank!" 
       end
     end
   
@@ -36,7 +28,6 @@ class Admin::CategoriesController < ApplicationController
       @category.destroy
       respond_to do |format|
         format.html { redirect_to categories_url, notice: "Category was successfully destroyed." }
-        format.json { head :no_content }
       end
     end
   
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+    redirect_to exams_path if current_user
   end
 end
